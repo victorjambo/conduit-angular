38f340a7d4c8ce7df2e9a2e4d0cfb8ec8c21ce02
victorjambo <victorjambo@live.com>
2019-03-14 01:09:23 +0300

    feat(article): generate article component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ArticleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
