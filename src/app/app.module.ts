82096e66c0cce2a6989d23ad81c116ed68521cfa
victorjambo <victorjambo@live.com>
2019-03-14 01:25:13 +0300

    feat(navbar): generate navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    ArticleComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
